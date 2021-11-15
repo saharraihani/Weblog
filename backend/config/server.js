77fe8cfd350e6346a4cdb9a15eb4c856a17ce057
Sahar Raihani <s.reyhani2001@gmail.com>
2021-11-15 09:20:06 +0430

    initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e3484b0a14df0daec71fc6273fca5205'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ba12f226-5c97-4f0f-9f7c-36a3c0343dce'
+};
